845ce1be80e2c9a11ccb0445a3692bbc88f615bd
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-12 10:13:57 -0300

    aula 1

---
+++ Aula1.java
@@ -0,0 +1,5 @@
+public class Aula1 {
+	public static void main(String[] args){
+		System.out.println("Primeira aula mt foda" + args[0]);
+	}
+}
---
+++ aula001/src/aula001/Computador.java
@@ -0,0 +1,5 @@
+package aula001;
+
+public class Computador {
+	String 
+}
